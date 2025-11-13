5de699c4a36a81ca6d01e7fb8fa311af020760b8
moxDroid Labs Inc <pritam.world@gmail.com>
2025-11-13 13:45:26 -0500

    first commit

---
+++ app/test_app.py
@@ -0,0 +1,7 @@
+from app.app import app
+
+def test_hello():
+    with app.test_client() as c:
+        rv = c.get("/")
+        assert rv.status_code == 200
+        assert rv.get_json()["message"] == "hello, world"
